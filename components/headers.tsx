4fa27279b5fa11cdec37b87982fd23ed60eb8616
ghilomdev <ghilomyonathan@gmail.com>
2024-09-04 21:36:03 -0700

    refactor: Update headers.tsx to use stable caching for navbar data

--- components/headers.tsx
+++ components/headers.tsx
@@ -1,4 +1,4 @@
-export const dynamic = 'force-dynamic';
+export const revalidate = 0 
 export const dynamicParams = true;
 import { contentfulClient } from "../services/get-or-update";
 import   renderLinks from "./nav";
@@ -9,17 +9,17 @@ const randomNavBarNames: string[] = ["Home", "About", "Services", "Contact"];
 export const Header = async () => {
    
 
-    // const getNavBar = unstable_cache(
-    //     async () => {
-          
-    //     },
-    //     ['NavHeader'],
-    //     {tags: ['NavHeader'] }
-    //   )
-    const datas:any =  await contentfulClient.getEntries({
-        content_type: "listMenu",
-        "fields.name": "Main Menu",
-    });
+    const getNavBar = unstable_cache(
+        async () => {
+          return await contentfulClient.getEntries({
+            content_type: "listMenu",
+            "fields.name": "Main Menu",
+        });
+        },
+        ['NavHeader'],
+        { revalidate: 0 }
+      )
+    const datas:any = await getNavBar();
     const links = datas.items[0].fields.listMenu;
     return (
         <nav className="mt-10">
