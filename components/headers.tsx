78da3754306a3f3606f23150fc153d8209747e4d
ghilomdev <ghilomyonathan@gmail.com>
2024-09-04 18:10:35 -0700

    force static

--- components/headers.tsx
+++ components/headers.tsx
@@ -1,4 +1,4 @@
-
+export const dynamic = 'force-static';
 export const dynamicParams = true;
 import { contentfulClient } from "../services/get-or-update";
 import   renderLinks from "./nav";
@@ -8,10 +8,7 @@ const randomNavBarNames: string[] = ["Home", "About", "Services", "Contact"];
 
 export const Header = async () => {
    
-    // const data = await contentfulClient.getEntries({
-    //     content_type: "listMenu",
-    //     "fields.name": "Main Menu",
-    // });
+
     const getNavBar = unstable_cache(
         async () => {
           return await contentfulClient.getEntries({
